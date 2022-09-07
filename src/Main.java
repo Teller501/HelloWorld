f821cb71666fbd477aa233fd9d313019cd17fa25
Teller501 <ande777j@stud.kea.dk>
2022-09-07 16:23:56 +0200

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hej Verden");
+        System.out.println("Anders");
+        System.out.println("Teller");
+        System.out.println("KOM NU LIDT");
+    }
+}
