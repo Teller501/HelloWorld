58ec552e7e6493883921a13840c5fcc476322fcb
Teller501 <ande777j@stud.kea.dk>
2022-09-09 13:02:31 +0200

    Revert "First Commit"
    This reverts commit 453932a88a0effe67ccf74b3441a9746b2d9e27b.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej Verden");
         System.out.println("Anders");
-        System.out.println("");
+        System.out.println("Teller");
         System.out.println("KOM NU LIDT");
     }
 }
